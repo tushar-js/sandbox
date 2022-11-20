f8642dab168b21a6bcdd80dc048b27abcd787e6c
Tushar Raj <tushar.raj@outlook.com>
2022-11-20 10:46:47 +0530

    router and login page

---
+++ test/src/pages/login.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+function Login() {
+
+    const loginFormInitial = {
+        email: 'qwerty@example.com',
+        password: '1234'
+    }
+
+    const [loginFormValue, setLoginFormValue] = useState(loginFormInitial);
+
+    const handleSubmit = () => {
+        console.log('handled')
+    }
+    const handleChange = (event, fieldName) => {
+        let val = event.target.value;
+
+        setLoginFormValue({ ...loginFormValue, [fieldName]: val })
+        console.log(loginFormValue)
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>ß
+            <label>
+                Email:
+                <input
+                    type="email"
+                    onChange={e => handleChange(e, 'email')}
+                    value={loginFormValue?.email || ''}
+                ></input>
+            </label>
+            <br />
+            <br />
+            <label>
+                Password:
+                <input
+                    type="password"
+                    onChange={e => handleChange(e, 'password')}
+                    value={loginFormValue?.password || ''}
+                ></input>
+            </label>
+            <br />
+            <br />
+            <input type="submit" value="Log In" />
+        </form>
+    )
+}
+
+export default Login;
