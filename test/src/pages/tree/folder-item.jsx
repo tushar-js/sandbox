236c60cda4012ff2a4687925ade2124a7d53d86b
Tushar Raj <tushar.raj@outlook.com>
2022-11-27 20:43:07 +0530

    folder recursive

--- test/src/pages/tree/folder-item.jsx
+++ test/src/pages/tree/folder-item.jsx
@@ -1,6 +1,28 @@
-const FolderItem = (props) => {
+import { useEffect } from "react";
+import FileItem from "./file-item";
+
+const FolderItem = ({ data }) => {
+
+    useEffect(() => {
+        console.log(data)
+    }, [])
+
+    let folderName = Object.keys(data)[0];
+    let folderItems = Object.values(data)[0] || [];
+
     return (
-        <p>{props.folderName}</p>
+        <>
+            <h4>{folderName}</h4>
+            {folderItems.map(item => {
+                if (typeof (item) == "string") {
+                    return <FileItem fileName={item}></FileItem>
+                } else {
+                    return <FolderItem data={item}></FolderItem>
+
+                }
+            })}
+
+        </>
     )
 }
 
--- test/src/pages/tree/tree.page.js
+++ test/src/pages/tree/tree.page.js
@@ -1,32 +1,16 @@
 
-import { useEffect } from 'react';
 import fileSystem from '../../data/file-system.json'
-import FileItem from './file-item';
 import FolderItem from './folder-item';
 
 function Tree() {
-    useEffect(()=> {
-        console.log('start');
-    }, [])
 
     const root = fileSystem.root || [];
 
-    return root.map(item => {
-        console.log(item)
-        if(typeof(item) == "string") {
-            return (
-                <FileItem
-                    fileName={item}
-                ></FileItem>
-            )
-        } else {
-            return (
-                <FolderItem
-                    folderName={Object.keys(item)[0]}
-                ></FolderItem>
-            )
-        }
-    })
+    return (
+        <FolderItem
+            data={fileSystem}
+        ></FolderItem>
+    )
 }
 
 export default Tree;
