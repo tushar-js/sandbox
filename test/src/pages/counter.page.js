f63f07362d7d83edab5c3e63bd7b065bc31bd6bc
Tushar Raj <tushar.raj@outlook.com>
2022-12-03 14:00:50 +0530

    RTK counter

--- test/src/pages/counter.page.js
+++ test/src/pages/counter.page.js
@@ -1,7 +1,27 @@
-const Counter = (props) => {
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../features/counter/counterSlice";
+
+const Counter = () => {
+
+    const count = useSelector(state => state.counter.value);
+    const dispatch = useDispatch();
+
+
     return (
         <div>
-            Counter
+            <button
+                aria-label="Increment"
+                onClick={()=> dispatch(increment())}
+            >
+                Increment
+            </button>
+            <span>{count}</span>
+            <button
+                aria-label="Decrement"
+                onClick={()=> dispatch(decrement())}
+            >
+                Decrement
+            </button>
         </div>
     )
 }
